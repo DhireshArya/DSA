200c40c13c3256b2fbd54ac40de06e4fe8839d54
DhireshArya <dhiresharya45@gmail.com>
2026-08-23 16:29:13 +0530

    Time: 0 ms (100%), Space: 9.6 MB (75.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,18 +3,33 @@ public:
     bool isAnagram(string s, string t) {
         int n = s.size();
         int m = t.size();
-        if(m!=n){
+
+        if(n!=m){
             return false;
         }
-
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-
-        for(int i=0; i<n; i++){
-            if(s[i]!=t[i]){
-                return false;
+        else{
+            // for the first string
+            int arr1[26] = {0};
+            for(int i=0; i<n; i++){
+                int b = s[i]-'a';
+                arr1[b] = arr1[b] + 1;
             }
+
+            // for the second string
+            int arr2[26] = {0};
+            for(int i=0; i<m; i++){
+                int b = t[i]-'a';
+                arr2[b] = arr2[b] + 1;
+            }
+
+            for(int i=0; i<26; i++){
+                if(arr1[i]!=arr2[i]){
+                    return false;
+                }
+            }
+
+            return true;
         }
-        return true;
+        
     }
 };
