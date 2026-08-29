38ca607484604d580ea3937ca466b22ac06133f3
DhireshArya <dhiresharya45@gmail.com>
2026-08-29 15:18:38 +0530

    Time: 0 ms (100%), Space: 15.1 MB (99.52%) - LeetHub

---
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -0,0 +1,112 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void leftSubtree(TreeNode* root,
+                 vector<pair<int, pair<int,int>>> &a,
+                 pair<int, pair<int,int>> p){
+
+        if(root == NULL){
+            return;
+        }
+
+        p.first = root->val;
+        a.push_back(p);
+
+        // coordinates for left child
+        pair<int,pair<int,int>> left = p;
+        left.second.first++;
+        left.second.second--;
+
+        // coordinates for right child
+        pair<int,pair<int,int>> right = p;
+        right.second.first++;
+        right.second.second++;
+
+        leftSubtree(root->left, a, left);
+        leftSubtree(root->right, a, right);
+    }
+
+
+    void rightSubtree(TreeNode* root,
+                  vector<pair<int, pair<int,int>>> &a,
+                  pair<int, pair<int,int>> p){
+
+        if(root == NULL){
+            return;
+        }
+
+        p.first = root->val;
+        a.push_back(p);
+
+        // coordinates for left child
+        pair<int,pair<int,int>> left = p;
+        left.second.first++;
+        left.second.second--;
+
+        // coordinates for right child
+        pair<int,pair<int,int>> right = p;
+        right.second.first++;
+        right.second.second++;
+
+        rightSubtree(root->left, a, left);
+        rightSubtree(root->right, a, right);
+    }
+
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        if(root==NULL){
+            return {};
+        }
+
+        vector<pair<int, pair<int,int>>> a;
+
+        pair<int,pair<int,int>> rootPos = {root->val,{0,0}};
+        a.push_back(rootPos);
+
+        // Pass the correct coordinates of children
+        leftSubtree(root->left, a, {0,{1,-1}});
+        rightSubtree(root->right, a, {0,{1,1}});
+
+        sort(a.begin(),a.end(),[](auto &x, auto &y){
+
+            if(x.second.second != y.second.second)
+                return x.second.second < y.second.second;
+
+            if(x.second.first != y.second.first)
+                return x.second.first < y.second.first;
+
+            return x.first < y.first;
+        });
+
+
+        // code for finding the level of the binary tree
+        vector<vector<int>> ans;
+        vector<int> col;
+
+        int prevCol = a[0].second.second;
+
+        for(auto &it : a){
+
+            if(it.second.second != prevCol){
+                ans.push_back(col);
+                col.clear();
+                prevCol = it.second.second;
+            }
+
+            col.push_back(it.first);
+        }
+
+        ans.push_back(col);
+
+        return ans;
+    }
+};
